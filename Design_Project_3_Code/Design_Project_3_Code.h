9cad4936d7699b1611b2d9c938088fd87fecf461
Mark-Lawrence22 <marklawrenceabela@gmail.com>
2025-06-13 22:14:19 +0800

    Add files via upload

---
+++ Design_Project_3_Code/Design_Project_3_Code.h
@@ -0,0 +1,15 @@
+// utils.h
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void printQR(const char *deviceName);  // Declare the function
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // UTILS_H
